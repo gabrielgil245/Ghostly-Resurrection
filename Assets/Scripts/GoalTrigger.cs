26695424a49af123a6749cb6edd69ba9b52d39e6
Gabriel Gil <gabrielgil245@gmail.com>
2022-05-26 22:39:03 -0500

    add goal trigger to navigate to next level

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") {
+            Debug.Log("Goal!");
+        }
+    }
+}
