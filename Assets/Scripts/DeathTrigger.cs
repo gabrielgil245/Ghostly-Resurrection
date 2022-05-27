be79d2377d821e938f85932f685222588b4d1186
Gabriel Gil <gabrielgil245@gmail.com>
2022-05-26 22:43:32 -0500

    chore: death trigger script clean up

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class DeathTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player") {
-            if(other.GetComponent<PlayerController>()._isGhost == false) {
-                other.GetComponent<PlayerController>()._isGhost = true;
+            PlayerController player = other.GetComponent<PlayerController>();
+            if(player._isGhost == false) {
+                player._isGhost = true;
             } else {
                 Debug.Log("Player is officially dead...");
             }
