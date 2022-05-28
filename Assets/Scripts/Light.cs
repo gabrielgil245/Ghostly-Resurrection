f8adc2d5342b5518a6b23952486d34cb1552adc1
Gabriel Gil <gabrielgil245@gmail.com>
2022-05-28 00:45:18 -0500

    fix: address nullpointer exception

--- Assets/Scripts/Light.cs
+++ Assets/Scripts/Light.cs
@@ -49,7 +49,7 @@ public class Light : MonoBehaviour
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        if(other.tag == "Player") {
+        if(other.tag == "Player" && other.gameObject.activeSelf) {
             _playerSprite.color = Color.red;
         }
     }
