b67c777acbd97d65cc3f833cb0417493e910331b
Gabriel Gil <gabrielgil245@gmail.com>
2022-05-27 22:53:03 -0500

    set trigger to reset if player is already a ghost

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathTrigger : MonoBehaviour
 {
+    [SerializeField] float _levelLoadDelay = 2f;
+    [SerializeField] float _levelExitSlowMoFactor = 0.2f;
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player") {
             PlayerController player = other.GetComponent<PlayerController>();
-            if(player._isGhost == false) {
+            if(!player._isGhost) {
                 player._isGhost = true;
             } else {
-                Debug.Log("Player is officially dead...");
+                player._isReallyDead = true;
                 other.gameObject.SetActive(false);
+                StartCoroutine(ResetLevel());
             }
         }
+    }
+
+    IEnumerator ResetLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Time.timeScale = _levelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(_levelLoadDelay);
+        
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
